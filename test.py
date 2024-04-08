e34104214631e40cbaeadbad8fce20b03d981bd2
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-04-08 22:50:46 +0200

    WIP2
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import subprocess
 import sys
-import toml
 
 def builds():
-	p = subprocess.run(['cargo', 'remote', '--no-copy-lock', '--', 'b', '-p', 'minimal-template-runtime'])
+	p = subprocess.run(['cargo', 'remote', '--no-copy-lock', '--', 'b', '-p', 'minimal-template-runtime'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	return p.returncode == 0
 
 def test():
 	# One-by one uncomment every line that starts with '#"' and run the build command. Record any
 	# lines that cause a failure. Then undo that replacement and proceed with the next line.
 
-	if not build():
+	print("Initial check")
+	if not builds():
 		raise Exception("Build failed")
 	
 	bad_lines = []
@@ -19,24 +19,22 @@ def test():
 			content = f.read()
 			lines = content.split('\n')
 		# Ensure that all bad lines are commented out
+		for i in bad_lines:
+			if '#"' not in lines[i]:
+				lines[i] = f'#{lines[i].strip()}'
 		for i, line in enumerate(lines):
-			if not line.contains('#"'):
-				lines[i] = '#"' + line
-		for i, line in enumerate(lines):
-			if not line.contains('#"'):
+			if '#"' not in line:
 				continue
 			if i in bad_lines:
 				continue
 			else:
-				lines[i] = line.replace('#"', '')
+				lines[i] = line.replace('#', '')
 				with open('umbrella/Cargo.toml', 'w') as f:
 					f.write('\n'.join(lines))
-		if builds():
-			print(f"Line is good {i}")
-		else:
-			print(f"Line is bad {i}: {line}")
+				break
+		if not builds():
+			print(f"🔥 Line is bad  {i}: {line}")
 			bad_lines.append(i)
-				
 
 if __name__ == '__main__':
 	test()
