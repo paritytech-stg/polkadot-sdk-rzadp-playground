9959e1030bcac26205d13cc616a3e63092a2ac44
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-04-08 22:41:40 +0200

    WIP2
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

---
+++ test.py
@@ -0,0 +1,42 @@
+import subprocess
+import sys
+import toml
+
+def builds():
+	p = subprocess.run(['cargo', 'remote', '--no-copy-lock', '--', 'b', '-p', 'minimal-template-runtime'])
+	return p.returncode == 0
+
+def test():
+	# One-by one uncomment every line that starts with '#"' and run the build command. Record any
+	# lines that cause a failure. Then undo that replacement and proceed with the next line.
+
+	if not build():
+		raise Exception("Build failed")
+	
+	bad_lines = []
+	while True:
+		with open('umbrella/Cargo.toml', 'r') as f:
+			content = f.read()
+			lines = content.split('\n')
+		# Ensure that all bad lines are commented out
+		for i, line in enumerate(lines):
+			if not line.contains('#"'):
+				lines[i] = '#"' + line
+		for i, line in enumerate(lines):
+			if not line.contains('#"'):
+				continue
+			if i in bad_lines:
+				continue
+			else:
+				lines[i] = line.replace('#"', '')
+				with open('umbrella/Cargo.toml', 'w') as f:
+					f.write('\n'.join(lines))
+		if builds():
+			print(f"Line is good {i}")
+		else:
+			print(f"Line is bad {i}: {line}")
+			bad_lines.append(i)
+				
+
+if __name__ == '__main__':
+	test()
